f93ea4288ba4ea28b9b39e8c34b3119a7846c1e0
Adrian Willian Frasson <adrianwillian.frasson@gmail.com>
2025-06-16 00:46:39 -0300

    unclutter - fix

--- __linux__/services/skynet_kiosk.sh
+++ __linux__/services/skynet_kiosk.sh
@@ -5,6 +5,6 @@ xset s noblank
 xset s off
 xset -dpms
 
-unclutter -idle 0.5 -root &
+unclutter --hide-on-touch --timeout 0 --root &
 
 chromium http://localhost/IHM --kiosk --noerrdialogs --disable-infobars --disable-session-crashed-bubble
